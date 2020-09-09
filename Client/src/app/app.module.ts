553c1326d06be2056fe91bbb6826019bf5b901f8
David Troutt <dtroutt@protonmail.com>
2020-09-08 20:53:01 -0400

    CRUD Cat + Pipe

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CreatepostComponent } from './createpost/createpost.component';
+import { CatService } from './services/cat.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { ListCatsComponent } from './list-cats/list-cats.component';
+import { DetailsCatComponent } from './details-cat/details-cat.component';
+import { EditCatComponent } from './edit-cat/edit-cat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreatepostComponent,
+    ListCatsComponent,
+    DetailsCatComponent,
+    EditCatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    AuthService, 
+    CatService, 
+    AuthGuardService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
