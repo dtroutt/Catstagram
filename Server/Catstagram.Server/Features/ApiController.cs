47ff5a5b1fda3e658e0a90501487f1e0853a5af1
David Troutt <dtroutt@protonmail.com>
2020-09-13 20:13:27 -0400

    Created features folder and added swagger

---
+++ Server/Catstagram.Server/Features/ApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace Catstagram.Server.Features
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+    }
+}
---
+++ Server/Catstagram.Server/Features/Cats/CreateCatRequestModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Catstagram.Server.Features.Cats
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.Validation.Cat;
+
+    public class CreateCatRequestModel
+    {
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [MaxLength(MaxDescriptionLength)]
+        public string Description { get; set; }
+    }
+}
